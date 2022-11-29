7751d1a0ea8344bd72db206f12d93d36bb706bdf
Amanda <bienz2@illinois.edu>
2022-11-29 15:06:00 -0800

    Added alltoall tests

---
+++ benchmarks/tioga/test_alltoall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export MPICH_GPU_SUPPORT_ENABLED=1
+
+cd /g/g14/bienz1/BenchPress/tioga_build/examples
+
+flux mini run --setop=mpibind=off -n 256 --verbose --exclusive --nodes=4 ./time_alltoall
